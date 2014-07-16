4fc166725031366ed7fa7b45b2438e10dad8d8ec
Lindsay-R <lrecknell@gmail.com>
2014-07-16 17:09:49 -0600

    student lead warmup review

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ What is going on??? Can you check this out for me?
 
 The note isn't very helpful. We have tried to reach out to the client but they aren't responding.
 Can you look into this?
+
+
+#### The problem is that if a user submits the form without the check box checked the program blows up. 
+#### If you don't check a check box then no "params" are sent to the server
+#### to solve you can redirect to the form and show an error
+#### ...or you can have an uncheck box send a false responce???
+
+
+#### more about debugging= http://pryrepl.org/
--- app.rb
+++ app.rb
@@ -21,10 +21,30 @@ class App < Sinatra::Base
   end
 
   post "/registrations" do
+
+    #--------added elsif statement-------
+    post "/registrations" do
+      if params[:name_is_hunter] == "0"
+        flash[:error] = "Please fill in all fields."
+        redirect "/register"
+
+      elsif  @database_connection.sql("SELECT * FROM users WHERE username = '#{params[:username].downcase}'") != []
+        flash[:error] = "Username is already taken."
+        redirect "/register"
+
+      end
+
+    #--------end elsif statement-------
+
+
     insert_sql = <<-SQL
       INSERT INTO users (username, email, password, name_is_hunter)
       VALUES ('#{params[:username]}', '#{params[:email]}', '#{params[:password]}', '#{params[:name_is_hunter]}')
     SQL
+
+    puts "="*20
+    puts insert_sql
+    puts "="*20
 
     @database_connection.sql(insert_sql)
     flash[:notice] = "Thanks for signing up"
